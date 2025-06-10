655184e524a32fe24ae0e12bf759599310cd18d0
Mkikii <maureenwkarimi@gmail.com>
2025-06-10 21:51:02 +0300

    Complete math lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// 1) Create a variable called multiply set to an equation that will multiply num1 and num2 to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
+// 2) Create a variable called random that generates a random integer > 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// 3) Create a variable called mod that calculates remainder of num3 divided by num4 to get 4
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4;
+
+// 4) Create a variable called max that finds the highest number in a set (20)
+const max = Math.max(5, 10, 15, 20);
 // Write your code here
